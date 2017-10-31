6670465c87ff9ef929cd7c471ef7295daf45309d
Alvin Rodis <alrodis147@gmail.com>
2017-10-30 20:59:29 -0500

    10-30 commit_3

--- liri.js
+++ liri.js
@@ -8,32 +8,39 @@ var twitterKeysINeed = require("./keys.js");
 //code to grab the user-input title of either the "song" or "movie" from command line (index 3), which will be passed into search
 var nodeArgs = process.argv;
 var title = "";
-for (var i = 2; i < nodeArgs.length; i++) {
+for (var i = 3; i < nodeArgs.length; i++) {
     title = title + " " + nodeArgs[i];
 };
-console.log("-------------------------");
-console.log("This is either the song or movie title the user had input: " + title);
-console.log("-------------------------");
+// console log test to ensure the "title" was grabbed successfully
+// console.log("-------------------------");
+// console.log("This is either the song or movie title the user had input: " + title);
+// console.log("-------------------------");
 
 //Switch statement to invoke functions based upon a specific command given for index 2 on command line
 var action = process.argv[2];
 
-switch (action) {
-    case "my-tweets":
-        myTweets();
-        break;
+function liriCommand(action, title) {
+    switch (action) {
+        case "my-tweets":
+            myTweets();
+            break;
 
-        // case "spotify-this-song"
-        // 	spotifySearch();
-        // 	break;
+        case "spotify-this-song":
+            spotifySearch();
+            break;
 
-        // case "movie-this";
-        // movieSearch();
-        // break;
+        case "movie-this":
+            movieSearch();
+            break;
 
-        // case "do-what-it-says";
-        // textSearch();
-        // break;
+        default:
+            console.log("Sorry not a recognized command.  Please enter a correct command.");
+
+        case "do-what-it-says":
+            textSearch();
+            break;
+    }
+
 }
 
 //Twitter
@@ -45,7 +52,7 @@ function myTweets() {
         if (!error) {
             // var tweetResponse = JSON.stringify(tweets, null, 2)
             for (var i = 0; i < tweets.length; i++) {
-                // console.log("-------------------------");
+                console.log("-------------------------");
                 console.log(tweets[i].text);
                 console.log(tweets[i].created_at);
                 console.log("-------------------------");
@@ -57,53 +64,63 @@ function myTweets() {
 }
 
 //Spotify
-var spotify = new Spotify({
-    id: '5195314043b341fea99001c41b8533e8',
-    secret: '467957e6ec6d451e963e4ba39c8846c4'
-});
+function spotifySearch() {
+    var spotify = new Spotify({
+        id: '5195314043b341fea99001c41b8533e8',
+        secret: '467957e6ec6d451e963e4ba39c8846c4'
+    });
 
-//Spotify "Search" method:
-spotify.search({ type: 'track', query: 'all the small things' }, function(err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-    // **console.log(data);  //all data returned
-    // **console.log(data.tracks.items[0]); //opening up data to get needed info
-    console.log("-------------------------");
-    console.log("Artist name: " + data.tracks.items[0].artists[0].name); //artist name
-    console.log("Song name: " + data.tracks.items[0].name); //song name
-    console.log("Preview link for this song: " + data.tracks.items[0].href); //preview link
-    console.log("Album name: " + data.tracks.items[0].album.name); //album name
-    console.log("-------------------------");
-});
+    //Spotify "Search" method:
+    spotify.search({ type: 'track', query: title }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // **console.log(data);  //all data returned
+        // **console.log(data.tracks.items[0]); //opening up data to get needed info
+        console.log("-------------------------");
+        console.log("Artist name: " + data.tracks.items[0].artists[0].name); //artist name
+        console.log("Song name: " + data.tracks.items[0].name); //song name
+        console.log("Preview link for this song: " + data.tracks.items[0].href); //preview link
+        console.log("Album name: " + data.tracks.items[0].album.name); //album name
+        console.log("-------------------------");
+    });
+
+}
 
 //Response/OMDB
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+function movieSearch() {
+    request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // console.log("-------------------------");
+            console.log(JSON.parse(body));
+            console.log("-------------------------");
+            console.log("Movie title: " + JSON.parse(body).Title);
+            console.log("Year Released: " + JSON.parse(body).Released);
+            console.log("This Movie's imdbRating is: " + JSON.parse(body).imdbRating);
+            console.log("This Movie's Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Source.Value); //still need to sort this one out
+            console.log("Country where produced: " + JSON.parse(body).Country);
+            console.log("The Movie's language is: " + JSON.parse(body).Language);
+            console.log("The Movie's plot is: " + JSON.parse(body).Plot);
+            console.log("Actors in this move are: " + JSON.parse(body).Actors);
+            console.log("-------------------------");
+        }
+    });
 
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
-        console.log("-------------------------");
-        console.log(JSON.parse(body));
-        console.log("-------------------------");
-        console.log("movie title: " + JSON.parse(body).title);
-        console.log("year released (dot): " + JSON.parse(body).released);
-        console.log("year released (bracket): " + JSON.parse(body)['released']);
-        console.log("The movie's imdbRating is: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes rating: " + JSON.parse(body).ratings);
-        console.log("Country produced: " + JSON.parse(body).country);
-        console.log("movie's language: " + JSON.parse(body).language);
-        console.log("movie plot: " + JSON.parse(body).plot);
-        console.log("movie actors: " + JSON.parse(body).actors);
-        console.log("-------------------------");
-        // console.log(response);
-    }
-});
+}
 
 //readFile to read contents of random.txt file, then need to take that content for the searchSpotify function
-fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error) {
-        return console.log(error);
-    }
-    console.log("Contents of random.txt file: " + data);
-    var dataArr = data.split(",");
-});
+function textSearch() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Contents of random.txt file: " + data)
+        var dataArr = data.split(",");
+        action = dataArr[0];
+        title = dataArr[1];
+        liriCommand(action, title);
+        return;
+    });
+}
+
+liriCommand(action, title);
